c53bedd8c4b025171be899d1a2769784830ccfca
paocai666 <3305188723@qq.com>
2025-03-04 22:55:52 +0800

    Add files via upload

---
+++ 阶乘求和(循环嵌套).cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int sum = 0;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		int p = 1;
+		for (int j = 1; j <= i; j++)
+		{
+			p = p * j;
+		}
+		sum = sum + p;
+	}
+	printf("答案是：%d", sum);
+	return 0;
+}
---
+++ 阶乘求和.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()
+{
+	int sum = 0;
+	int n = 0;
+	scanf_s("%d", &n);
+	int p = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		p = p * i;
+		sum = sum + p;
+	}
+	printf("答案是：%d", sum);
+	return 0;
+}
